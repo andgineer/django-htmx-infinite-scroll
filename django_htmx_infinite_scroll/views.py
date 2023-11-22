136b14970d39917caaa029bc0f2a913a1f3d0189
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2023-11-22 11:27:51 +0100

    removed un-working code

--- django_htmx_infinite_scroll/views.py
+++ django_htmx_infinite_scroll/views.py
@@ -8,24 +8,22 @@ from .models import BookPage
 
 def book(request: HttpRequest) -> HttpResponse:
     """Fetch paginated book and render only visible pages."""
-    page_number = request.GET.get("book-page", 1)
+    page_number = request.GET.get("page-number", 1)
     page = BookPage.objects.get(number=page_number)
     return render(request, "book.html", {"page": page})
 
 
 def book_page(request: HttpRequest) -> HttpResponse:
     """Render the book page."""
-    page_number = request.GET.get("book-page", 1)
+    page_number = request.GET.get("page-number", 1)
     try:
-        next_page = BookPage.objects.get(number=page_number)
+        page = BookPage.objects.get(number=page_number)
     except BookPage.DoesNotExist:
         return HttpResponse(f"error: Page {page_number} not found", status=500)
 
     # Render only the content of the next page as a partial HTML response
     content = render_to_string("page.html", {
-        "page": next_page,
-        "type": request.GET.get("type"),
-        "book_page": request.GET.get("book-page"),
+        "page": page,
     })
 
     return HttpResponse(content)
